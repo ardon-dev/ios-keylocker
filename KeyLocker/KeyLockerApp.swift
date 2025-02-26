af2993fadac2b76cfc2dc57ebecf5809af1d49a7
Ardón <josueardon.developer@gmail.com>
2025-02-26 14:16:47 -0600

    Initial Commit

---
+++ KeyLocker/KeyLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyLockerApp.swift
+//  KeyLocker
+//
+//  Created by Josue on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct KeyLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KeyLockerTests/KeyLockerTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeyLockerTests.swift
+//  KeyLockerTests
+//
+//  Created by Josue on 26/2/25.
+//
+
+import Testing
+@testable import KeyLocker
+
+struct KeyLockerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
